f1d33eb11fc55917294177fcdc419c04760dd985
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-11-30 18:54:23 -0300

    Monitorando os nossos sites

--- hello.go
+++ hello.go
@@ -9,21 +9,23 @@ import (
 func main() {
 
     exibeIntroducao()
-    exibeMenu()
-	comando := leComando()
+    for {
+		exibeMenu()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-        iniciarMonitoramento()
-    case 2:
-        fmt.Println("Exibindo logs...")
-    case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-    default:
-        fmt.Println("Não conheço este comando")
-        os.Exit(-1)
-	}
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}		
+	}    
 }
 
 func exibeIntroducao() {
@@ -51,5 +53,10 @@ func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
 	site := "https://www.alura.com.br"
 	resp, _ := http.Get(site)
-	fmt.Println(resp)
+	
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")		
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
