080496b82b090d95d45602f4df3a68ef64b192c1
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-12-01 00:46:23 -0300

    Testando múltiplas vezes

--- hello.go
+++ hello.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"os"
 	"net/http"
+	"time"
 )
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 
@@ -38,25 +42,42 @@ func exibeIntroducao() {
 func exibeMenu() {
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibir logs")
-	fmt.Println("0 - Sair do programa")	
+	fmt.Println("0 - Sair do programa")
+	fmt.Print("Opção: ")	
 }
 
 func leComando () int {
     var comandoLido int
-    fmt.Scan(&comandoLido)
-    fmt.Println("O comando esolhido foi: ", comandoLido)
+    fmt.Scan(&comandoLido)   
 
     return comandoLido
 }
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://www.alura.com.br"
+	sites := []string{
+		"https://www.alura.com.br",
+		"https://www.casadocodigo.com.br",
+		"https://www.ead.senac.br/"}
+		
+	for i:= 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i+1, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+	}
+	
+	fmt.Println("")
+	
+}
+
+func testaSite(site string) {
 	resp, _ := http.Get(site)
 	
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")		
 	} else {
 		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
-	}
+	}	
 }
